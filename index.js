f29b92201501d3b7b6af906d7c3b6a043cae8c58
jvictormorais <jose.alvarezmorais@gmail.com>
2023-01-30 15:46:12 -0300

    Add style

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const playerText = document.querySelector("#playerText");
 const computerText = document.querySelector("#computerText");
 const resultText = document.querySelector("#resultText");
+const scoreText = document.querySelector("#scoreText");
 const choiceBtns = document.querySelectorAll(".choiceBtn");
 let player;
 let computer;
 let result;
+let playerScore = 0;
+let computerScore = 0;
 
 choiceBtns.forEach(button => button.addEventListener("click", () => {
 
     player = button.textContent;
-    computerTurn();
+    computerTurn(); checkScore();
     playerText.textContent = `Player: ${player}`;
     computerText.textContent = `Computer: ${computer}`;
     resultText.textContent = checkWinner();
+    scoreText.textContent = `${playerScore} Player vs Computer ${computerScore}`;
+
 }))
 
 function computerTurn(){
@@ -36,12 +41,27 @@ function checkWinner (){
         return "Draw!";
     }
     else if(computer == "ROCK"){
-        return(player == "PAPER") ? "You Win!" : "You Lose!"
+        return(player == "PAPER") ? "You Win!"  : "You Lose!";
     }
     else if(computer == "PAPER"){
         return(player == "SCISSORS") ? "You Win!" : "You Lose!"
     }
     else if(computer == "SCISSORS"){
         return(player == "ROCK") ? "You Win!" : "You Lose!"
+    }
+}
+function checkScore (){
+    if(player == computer){
+        computerScore +=0;
+        playerScore +=0;
+    }
+    else if(computer == "ROCK"){
+        return(player == "PAPER") ? playerScore +=1  : computerScore +=1;
+    }
+    else if(computer == "PAPER"){
+        return(player == "SCISSORS") ? playerScore +=1 : computerScore +=1
+    }
+    else if(computer == "SCISSORS"){
+        return(player == "ROCK") ? playerScore +=1 : computerScore +=1
     }
 }
